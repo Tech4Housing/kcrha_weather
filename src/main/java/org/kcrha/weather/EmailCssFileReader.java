7683d908726681dbd4d37057680c641c3763ca03
Jeffrey Green <jeffrey.green.ii.2013@gmail.com>
2023-09-04 01:10:33 -0700

    file name change

--- src/main/java/org/kcrha/weather/EmailCssFileReader.java
+++ src/main/java/org/kcrha/weather/EmailCssFileReader.java
@@ -1,7 +1,7 @@
 package org.kcrha.weather;
 
 public class EmailCssFileReader extends BaseFileReader {
-    public static final String EMAIL_CSS_FILE_PATH = "emailStyle.css";
+    public static final String EMAIL_CSS_FILE_PATH = "style.css";
     public static String getStyle() {
 
         return EmailCssFileReader.readFile(EMAIL_CSS_FILE_PATH);
