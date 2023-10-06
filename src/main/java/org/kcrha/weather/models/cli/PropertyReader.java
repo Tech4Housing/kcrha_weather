c3784df92d60e39096b5e2fa1c8288ea4994314b
Jeffrey Green <jeffrey.green.ii.2013@gmail.com>
2023-10-06 14:05:25 -0400

    cleaning up property reader

--- src/main/java/org/kcrha/weather/models/cli/PropertyReader.java
+++ src/main/java/org/kcrha/weather/models/cli/PropertyReader.java
@@ -1,18 +1,22 @@
 package org.kcrha.weather.models.cli;
 
+import org.apache.commons.io.input.CharSequenceReader;
+import org.kcrha.weather.BaseFileReader;
+
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
-public class PropertyReader {
+public class PropertyReader extends BaseFileReader {
 
     private static final String SECRETS_PROPERTIES_FILE = "secrets.properties";
 
     public static Properties getSecretProperties() throws IOException {
+        String fileContents = readFile(SECRETS_PROPERTIES_FILE);
         Properties prop = new Properties();
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        InputStream stream = loader.getResourceAsStream(SECRETS_PROPERTIES_FILE);
-        prop.load(stream);
+        try (CharSequenceReader csr = new CharSequenceReader(fileContents)) {
+            prop.load(csr);
+        }
+
         return prop;
     }
 }
